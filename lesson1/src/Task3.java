d88d1a10f59e6f61a4dadfc3fdf5bd0d52151840
KozDaria <dmi7580@yandex.ru>
2022-11-18 19:10:42 +0300

    Added lesson1

---
+++ lesson1/src/Task3.java
@@ -0,0 +1,7 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int number = 10500;
+        double result = (number/10.) /10;
+        System.out.println(result);
+ }
+}
---
+++ lesson1/src/Task4.java
@@ -0,0 +1,6 @@
+public class Task4 {
+    public static void main(String[] args) {
+        double result = 3.6 * 4.1 * 5.9;
+        System.out.println(result);
+ }
+}
