4147a2303aaccffae9462bbfe8a9703c4841da26
KozDaria <dmi7580@yandex.ru>
2022-11-27 21:20:34 +0300

    fixed lab5 (refactored)

--- lesson5/palindrome/src/Palindrome.java
+++ lesson5/palindrome/src/Palindrome.java
@@ -2,16 +2,16 @@ public class Palindrome {
     public static void main(String[] args) {
         String word = "нежен";
         String phrase = "искать такси";
-        System.out.println(word.equalsIgnoreCase(revertWord(word)));
+        System.out.println((revertWord(word)));
         System.out.println(revertPhrase(phrase));
     }
 
-    private static String revertWord(String word) {
+    private static boolean revertWord(String word) {
         StringBuilder word2 = new StringBuilder();
         for (int i = word.length() - 1; i >= 0; i--) {
             word2.append(word.charAt(i));
         }
-        return word2.toString();
+        return word.equalsIgnoreCase(word2.toString());
     }
 
     private static boolean revertPhrase(String phrase) {
